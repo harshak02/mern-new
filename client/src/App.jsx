61caf5f2b14be4bc0414a1f74c1b3872cb24e1ec
harshak02 <sreeharshak002@gmail.com>
2024-04-07 10:21:01 +0530

    commit 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-red-500">Hello World</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
